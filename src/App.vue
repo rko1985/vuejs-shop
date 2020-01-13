9a8ff27ad0b8f34a55a142d9d0729e005e945f74
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-13 11:13:37 -1000

    creating product component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-md-7"></div>
+      <div class="col-md-7">
+        <div class="row">
+          <div :key="product.id" class="col-md-6" v-for="product in products">
+            <product></product>
+          </div>
+        </div>
+      </div>
       <div class="col-md-5"></div>
     </div>
   </div>
@@ -10,9 +16,19 @@
 </template>
 
 <script>
+import products from '@/products.json';
+import Product from '@/components/Product.vue';
 
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Product
+  },
+  data(){
+    return {
+      products
+    }
+  }
 
 }
 </script>
