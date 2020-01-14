99a9de44abf943c1cffef4ead2558b83742f00ec
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-14 07:28:46 -1000

    add to cart

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
           </div>
         </div>
       </div>
-      <div class="col-md-5"></div>
+      <div class="col-md-5 my-5">
+        <cart :items="cart"></cart>
+      </div>
     </div>
   </div>
   
@@ -18,11 +20,13 @@
 <script>
 import products from '@/products.json';
 import Product from '@/components/Product.vue';
+import Cart from '@/components/Cart.vue'
 
 export default {
   name: 'app',
   components: {
-    Product
+    Product,
+    Cart
   },
   data(){
     return {
