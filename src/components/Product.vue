8d6bfed812b5a1eae2b6b71661db3e4964c9f740
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-14 07:05:43 -1000

    disabling product button after adding to cart

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,13 +3,15 @@
         <div class="card-body">
             <h5 class="text-center card-title">{{product.title}}</h5>
             <p class="text-center text-muted card-text display-4">$ {{Number(product.price).toFixed()}}</p>
-            <button @click="$emit('add-to-cart', product)" class="btn btn-primary form-control">Add To Card</button>
+            <button :disabled="isInCart" @click="$emit('add-to-cart', product)" class="btn btn-primary form-control">
+                {{isInCart ? 'Added to cart.' : 'Add to cart'}}
+            </button>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['product']
+    props: ['product', 'isInCart']
 }
 </script>
